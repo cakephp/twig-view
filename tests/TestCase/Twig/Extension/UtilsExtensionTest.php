57c213f0e8471f28a96812dff5d9ba31e1f14997
Mark Story <mark@mark-story.com>
2026-06-20 00:19:16 -0400

    Add skip for php 8.2

--- tests/TestCase/Twig/Extension/UtilsExtensionTest.php
+++ tests/TestCase/Twig/Extension/UtilsExtensionTest.php
@@ -33,6 +33,8 @@ class UtilsExtensionTest extends AbstractExtensionTest
 
     public function testUnserializePreventObject(): void
     {
+        $this->skipIf(version_compare(PHP_VERSION, '8.3.0', '<'), 'Requires PHP8.3 or higher');
+
         $twig = new Environment(new ArrayLoader([
             // {% set %} so we exercise the filter without stringifying the result.
             'object' => '{% set _ = payload|unserialize %}(rendered)',
