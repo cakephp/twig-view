4cceed999d5d2f9e3bf5d04a885a493e4f6bbbc0
Péter Nagy <peter@antronin.consulting>
2026-08-18 15:02:20 +0200

    Overwrite 'i18n extract' to make it able to parse twig templates too.

---
+++ src/Command/TwigExtractCommand.php
@@ -0,0 +1,359 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
+ * @since         1.2.0
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+
+namespace Cake\TwigView\Command;
+
+use Cake\Command\I18nExtractCommand;
+use Cake\Command\Helper\ProgressHelper;
+use Cake\Console\Arguments;
+use Cake\Console\ConsoleIo;
+use Cake\Utility\Filesystem;
+use Twig\Token;
+use function count; // Imports the global function
+use function is_array; // Imports the global function
+use function in_array; // Imports the global function
+
+/**
+ * Language string extractor
+ */
+class TwigExtractCommand extends I18nExtractCommand
+{
+    /**
+     * @inheritDoc
+     */
+    public static function defaultName(): string
+    {
+        return 'i18n extract';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function getDescription(): string
+    {
+        return 'Extract i18n POT files from application files and twig templates.';
+    }
+
+    /**
+     * Extract tokens out of all files to be processed
+     *
+     * @param \Cake\Console\Arguments $args The io instance
+     * @param \Cake\Console\ConsoleIo $io The io instance
+     * @return void
+     */
+    protected function _extractTokens(Arguments $args, ConsoleIo $io): void
+    {
+        $progress = $io->helper('progress');
+        assert($progress instanceof ProgressHelper);
+        $progress->init(['total' => count($this->_files)]);
+        $isVerbose = $args->getOption('verbose');
+
+        $functions = [
+            '__' => ['singular'],
+            '__n' => ['singular', 'plural'],
+            '__d' => ['domain', 'singular'],
+            '__dn' => ['domain', 'singular', 'plural'],
+            '__x' => ['context', 'singular'],
+            '__xn' => ['context', 'singular', 'plural'],
+            '__dx' => ['domain', 'context', 'singular'],
+            '__dxn' => ['domain', 'context', 'singular', 'plural'],
+        ];
+        $pattern = '/(' . implode('|', array_keys($functions)) . ')\s*\(/';
+
+        foreach ($this->_files as $file) {
+            $this->_file = $file;
+            if ($isVerbose) {
+                $io->verbose(sprintf('Processing %s...', $file));
+            }
+            if (pathinfo($file, PATHINFO_EXTENSION) === 'twig') {
+                if ($this->_isTwigUsable()) {
+                    $_parser = 'twig';
+                } else {
+                    $io->warning('Twig is not installed. Please install Twig to extract translations from twig templates.');
+                    continue;
+                }
+                $_parser = 'twig';
+            } else {
+                $_parser = 'php';
+            }
+            $code = (string)file_get_contents($file);
+
+            if (preg_match($pattern, $code) === 1) {
+                if ($_parser === 'twig') {
+                    $this->_tokenizeAsTwig($code, $file);
+                } else {
+                    $this->_tokenizeAsPHP($code, $file);
+                }
+
+                foreach ($functions as $functionName => $map) {
+                    if ($_parser === 'twig') {
+                        $this->_parseAsTwig($io, $functionName, $map);
+                    } else {
+                        $this->_parseAsPHP($io, $functionName, $map);
+                    }
+                }
+            }
+
+            if (!$isVerbose) {
+                $progress->increment(1);
+                $progress->draw();
+            }
+        }
+    }
+
+    /**
+     * Parse tokens
+     *
+     * @param \Cake\Console\ConsoleIo $io The io instance
+     * @param string $functionName Function name that indicates translatable string (e.g: '__')
+     * @param array $map Array containing what variables it will find (e.g: domain, singular, plural)
+     * @return void
+     */
+    protected function _parseAsPHP(ConsoleIo $io, string $functionName, array $map): void
+    {
+        $count = 0;
+        $tokenCount = \count($this->_tokens);
+
+        while ($tokenCount - $count > 1) {
+            $countToken = $this->_tokens[$count];
+            $firstParenthesis = $this->_tokens[$count + 1];
+            if (!is_array($countToken)) {
+                $count++;
+                continue;
+            }
+
+            [$type, $string, $line] = $countToken;
+            if (($type === T_STRING) && ($string === $functionName) && ($firstParenthesis === '(')) {
+                $position = $count;
+                $depth = 0;
+
+                while (!$depth) {
+                    if ($this->_tokens[$position] === '(') {
+                        $depth++;
+                    } elseif ($this->_tokens[$position] === ')') {
+                        $depth--;
+                    }
+                    $position++;
+                }
+
+                $mapCount = count($map);
+                $strings = $this->_getStrings($position, $mapCount);
+
+                if ($mapCount === count($strings)) {
+                    $singular = '';
+                    $vars = array_combine($map, $strings);
+                    extract($vars);
+                    $domain ??= 'default';
+                    $details = [
+                        'file' => $this->_file,
+                        'line' => $line,
+                    ];
+                    $details['file'] = '.' . str_replace(ROOT, '', $details['file']);
+                    if (isset($plural)) {
+                        $details['msgid_plural'] = $plural;
+                    }
+                    if (isset($context)) {
+                        $details['msgctxt'] = $context;
+                    }
+                    $this->_addTranslation($domain, $singular, $details);
+                } else {
+                    $this->_markerError($io, $this->_file, $line, $functionName, $count);
+                }
+            }
+            $count++;
+        }
+    }
+
+    /**
+     * Parse Twig tokens
+     *
+     * @param \Cake\Console\ConsoleIo $io The io instance
+     * @param string $functionName Function name that indicates translatable string (e.g: '__')
+     * @param array $map Array containing what variables it will find (e.g: domain, singular, plural)
+     * @return void
+     */
+    protected function _parseAsTwig(ConsoleIo $io, string $functionName, array $map): void
+    {
+        /** @var \Twig\Token $token */
+        foreach ($this->_tokens as $count => $token) {
+            if ($token->test(Token::NAME_TYPE, $functionName)) {
+                switch ($functionName) {
+                    case '__':
+                        $singular = $this->_getStringFromToken($count, 2);
+                        break;
+                    case '__n':
+                        $singular = $this->_getStringFromToken($count, 2);
+                        $plural = $this->_getStringFromToken($count, 4);
+                        break;
+                    case '__d':
+                        $domain = $this->_getStringFromToken($count, 2);
+                        $singular = $this->_getStringFromToken($count, 4);
+                        break;
+                    case '__dn':
+                        $domain = $this->_getStringFromToken($count, 2);
+                        $singular = $this->_getStringFromToken($count, 4);
+                        $plural = $this->_getStringFromToken($count, 6);
+                        break;
+                    case '__x':
+                        $context = $this->_getStringFromToken($count, 2);
+                        $singular = $this->_getStringFromToken($count, 4);
+                        break;
+                    case '__xn':
+                        $context = $this->_getStringFromToken($count, 2);
+                        $singular = $this->_getStringFromToken($count, 4);
+                        $plural = $this->_getStringFromToken($count, 6);
+                        break;
+                    case '__dx':
+                        $domain = $this->_getStringFromToken($count, 2);
+                        $context = $this->_getStringFromToken($count, 4);
+                        $singular = $this->_getStringFromToken($count, 6);
+                        break;
+                    case '__dxn':
+                        $domain = $this->_getStringFromToken($count, 2);
+                        $context = $this->_getStringFromToken($count, 4);
+                        $singular = $this->_getStringFromToken($count, 6);
+                        $plural = $this->_getStringFromToken($count, 8);
+                        break;
+                }
+                $domain ??= 'default';
+                $details = [
+                    'file' => $this->_file,
+                    'line' => $token->getLine(),
+                ];
+                $details['file'] = '.' . str_replace(ROOT, '', $details['file']);
+                if (in_array('plural', $map)) {
+                    if (isset($plural)) {
+                        $details['msgid_plural'] = $plural;
+                    } else {
+                        $this->_markerError($io, $this->_file, $token->getLine(), $functionName, $token->getOffset());
+                        continue;
+                    }
+                }
+
+                if (in_array('context', $map)) {
+                    if (isset($context)) {
+                        $details['msgctxt'] = $context;
+                    } else {
+                        $this->_markerError($io, $this->_file, $token->getLine(), $functionName, $token->getOffset());
+                        continue;
+                    }
+                }
+                $this->_addTranslation($domain, $singular, $details);
+            }
+        }
+    }
+
+    /**
+     * Search files that may contain translatable strings
+     *
+     * @return void
+     */
+    protected function _searchFiles(): void
+    {
+        $pattern = false;
+        if ($this->_exclude) {
+            $exclude = [];
+            foreach ($this->_exclude as $e) {
+                if (DIRECTORY_SEPARATOR !== '\\' && !str_starts_with($e, DIRECTORY_SEPARATOR)) {
+                    $e = DIRECTORY_SEPARATOR . $e;
+                }
+                $exclude[] = preg_quote($e, '/');
+            }
+            $pattern = '/' . implode('|', $exclude) . '/';
+        }
+
+        foreach ($this->_paths as $path) {
+            $path = realpath($path);
+            if ($path === false) {
+                continue;
+            }
+            $path .= DIRECTORY_SEPARATOR;
+            $fs = new Filesystem();
+            $files = $fs->findRecursive($path, '/\.php$|\.twig$/');
+            $files = array_keys(iterator_to_array($files));
+            sort($files);
+            if ($pattern) {
+                $files = preg_grep($pattern, $files, PREG_GREP_INVERT) ?: [];
+                $files = array_values($files);
+            }
+            $this->_files = array_merge($this->_files, $files);
+        }
+        $this->_files = array_unique($this->_files);
+    }
+
+    /**
+     * Checks whether the Twig templating system is available.
+     *
+     * @return bool true if Twig is autoloadable and usable, false otherwise
+     */
+    protected function _isTwigUsable(): bool
+    {
+        if (!class_exists('Twig\Environment')) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Parses the given PHP source code for tokens, filtering out whitespace and inline HTML.
+     *
+     * @param string $code Source code of the file to parse
+     * @param string $file File name and path of the file to parse
+     * @return void
+     */
+    protected function _tokenizeAsPHP(string $code, string $file): void
+    {
+        $allTokens = token_get_all($code);
+        $this->_tokens = [];
+        foreach ($allTokens as $token) {
+            if (!is_array($token) || ($token[0] !== T_WHITESPACE && $token[0] !== T_INLINE_HTML)) {
+                $this->_tokens[] = $token;
+            }
+        }
+        unset($allTokens);
+    }
+
+    /**
+     * Parses the given Twig source code for tokens.
+     *
+     * @param string $code Source code of the file to parse
+     * @param string $file File name and path of the file to parse
+     * @return void
+     */
+    protected function _tokenizeAsTwig(string $code, string $file): void
+    {
+        $twig = new \Twig\Environment(new \Twig\Loader\ArrayLoader());
+        /**
+         * @var \Twig\TokenStream $stream
+         */
+        $stream = $twig->tokenize(new \Twig\Source(code: $code, name: $file, path: $file));
+        $this->_tokens = [];
+        while (!$stream->isEOF()) {
+            $token = $stream->next();
+            if (! $token->test(\Twig\Token::TEXT_TYPE) && ! $token->test(\Twig\Token::BLOCK_END_TYPE)) {
+                $this->_tokens[] = $token;
+            }
+        }
+        unset($stream);
+    }
+
+    protected function _getStringFromToken(int $position, int $offset):string {
+        $string = $this->_tokens[$position + $offset]->getValue();
+        return str_replace('"', '\"', $string);
+    }
+}
---
+++ tests/TestCase/Command/I18nExtractCommandTest.php
@@ -0,0 +1,244 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * CakePHP :  Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP Project
+ * @since         1.2.0
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+
+namespace Cake\TwigView\Test\TestCase\Command;
+
+use Cake\Console\TestSuite\ConsoleIntegrationTestTrait;
+use Cake\Core\Configure;
+use Cake\Routing\Router;
+use Cake\TestSuite\TestCase;
+use Cake\Utility\Filesystem;
+
+/**
+ * I18nExtractCommandTest
+ */
+class I18nExtractCommandTest extends TestCase
+{
+    use ConsoleIntegrationTestTrait;
+
+    /**
+     * @var string
+     */
+    protected $path;
+
+    /**
+     * setUp method
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->path = TMP . 'tests/extract_task_test';
+        $fs = new Filesystem();
+        $fs->deleteDir($this->path);
+        $fs->mkdir($this->path . DS . 'locale');
+
+        Router::reload();
+        Configure::write('App.encoding', 'UTF-8');
+
+        $this->loadPlugins(['Cake/TwigView']);
+        $this->setAppNamespace();
+    }
+
+    /**
+     * tearDown method
+     */
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        $fs = new Filesystem();
+        $fs->deleteDir($this->path);
+        $this->clearPlugins();
+    }
+
+    /**
+     * testExecute method
+     */
+    public function testExecute(): void
+    {
+        $this->exec(
+            'i18n extract ' .
+                '--merge=no ' .
+                '--extract-core=no ' .
+                '--paths=' . TEST_APP . 'templates' . DS . 'i18n ' . DS.
+                '--output=' . $this->path . DS,
+            [
+                $this->path,
+            ],
+        );
+        $this->assertExitSuccess();
+        $this->assertFileExists($this->path . DS . 'default.pot');
+        $this->assertFileExists($this->path . DS . 'test.pot');
+        $this->assertFileDoesNotExist($this->path . DS . 'cake.pot');
+
+        $result = file_get_contents($this->path . DS . 'default.pot');
+
+        // The additional "./tests/test_app" is just due to the wonky folder structure of the test app.
+        // In a regular app the path would start with "./templates".
+
+        // no_domain
+        $pattern = '@(\#: \./tests/test_app/templates/i18n/i18n_test\.twig:\d+\n)+';
+        $pattern .= 'msgid "no_domain"@';
+        $this->assertMatchesRegularExpression($pattern, $result);
+
+        // no_domain, var
+        $pattern = '@(\#: \./tests/test_app/templates/i18n/i18n_test\.twig:\d+\n)+';
+        $pattern .= 'msgid "no_domain_with_\{var\}"@';
+        $this->assertMatchesRegularExpression($pattern, $result);
+
+        // no_domain, var, context
+        $pattern = '@(\#: \./tests/test_app/templates/i18n/i18n_test\.twig:\d+\n)+';
+        $pattern .= 'msgctxt "Context"\n';
+        $pattern .= 'msgid "no_domain_with_context_with_\{var\}"@';
+        $this->assertMatchesRegularExpression($pattern, $result);
+
+        // no_domain, plural
+        $pattern = '@(\#: \./tests/test_app/templates/i18n/i18n_test\.twig:\d+\n)+';
+        $pattern .= 'msgid "no_domain_singular"\nmsgid_plural "no_domain_plural"@';
+        $this->assertMatchesRegularExpression($pattern, $result);
+
+        // no_domain, plural, context
+        $pattern = '@(\#: \./tests/test_app/templates/i18n/i18n_test\.twig:\d+\n)+';
+        $pattern .= 'msgctxt "Context"\n';
+        $pattern .= 'msgid "no_domain_singular_with_context"\nmsgid_plural "no_domain_plural_with_context"@';
+        $this->assertMatchesRegularExpression($pattern, $result);
+
+        // no_domain, plural, var
+        $pattern = '@(\#: \./tests/test_app/templates/i18n/i18n_test\.twig:\d+\n)+';
+        $pattern .= 'msgid "no_domain_singular_with_\{var\}"\nmsgid_plural "no_domain_plural_with_\{var\}"@';
+        $this->assertMatchesRegularExpression($pattern, $result, 'No duplicate msgid');
+
+        // no_domain, plural, var, context
+        $pattern = '@(\#: \./tests/test_app/templates/i18n/i18n_test\.twig:\d+\n)+';
+        $pattern .= 'msgctxt "Context"\n';
+        $pattern .= 'msgid "no_domain_singular_with_context_with_\{var\}"\nmsgid_plural "no_domain_plural_with_context_with_\{var\}"@';
+        $this->assertMatchesRegularExpression($pattern, $result, 'No duplicate msgid');
+
+
+        $this->assertStringContainsString('msgid "double \\"quoted\\""', $result, 'Strings with quotes not handled correctly');
+        $this->assertStringContainsString("msgid \"single 'quoted'\"", $result, 'Strings with quotes not handled correctly');
+
+        // test.pot
+        $result = file_get_contents($this->path . DS . 'test.pot');
+
+        // test_domain
+        $pattern = '@(\#: \./tests/test_app/templates/i18n/i18n_test\.twig:\d+\n)+';
+        $pattern .= 'msgid "test_domain"@';
+        $this->assertMatchesRegularExpression($pattern, $result);
+
+        // test_domain, var
+        $pattern = '@(\#: \./tests/test_app/templates/i18n/i18n_test\.twig:\d+\n)+';
+        $pattern .= 'msgid "test_domain_with_{var}"@';
+        $this->assertMatchesRegularExpression($pattern, $result);
+
+        // test_domain, var, context
+        $pattern = '@(\#: \./tests/test_app/templates/i18n/i18n_test\.twig:\d+\n)+';
+        $pattern .= 'msgctxt "Context"\n';
+        $pattern .= 'msgid "test_domain_with_context_with_\{var\}"@';
+        $this->assertMatchesRegularExpression($pattern, $result);
+
+        // test_domain, plural
+        $pattern = '@(\#: \./tests/test_app/templates/i18n/i18n_test\.twig:\d+\n)+';
+        $pattern .= 'msgid "test_domain_singular"\nmsgid_plural "test_domain_plural"@';
+        $this->assertMatchesRegularExpression($pattern, $result);
+
+        // test_domain, plural, context
+        $pattern = '@(\#: \./tests/test_app/templates/i18n/i18n_test\.twig:\d+\n)+';
+        $pattern .= 'msgctxt "Context"\n';
+        $pattern .= 'msgid "test_domain_singular_with_context"\nmsgid_plural "test_domain_plural_with_context"@';
+        $this->assertMatchesRegularExpression($pattern, $result);
+
+        // test_domain, plural, var
+        $pattern = '@(\#: \./tests/test_app/templates/i18n/i18n_test\.twig:\d+\n)+';
+        $pattern .= 'msgid "test_domain_singular_with_\{var\}"\nmsgid_plural "test_domain_plural_with_\{var\}"@';
+        $this->assertMatchesRegularExpression($pattern, $result, 'No duplicate msgid');
+
+        // test_domain, plural, var, context
+        $pattern = '@(\#: \./tests/test_app/templates/i18n/i18n_test\.twig:\d+\n)+';
+        $pattern .= 'msgctxt "Context"\n';
+        $pattern .= 'msgid "test_domain_singular_with_context_with_\{var\}"\nmsgid_plural "test_domain_plural_with_context_with_\{var\}"@';
+        $this->assertMatchesRegularExpression($pattern, $result, 'No duplicate msgid');
+
+        $this->assertStringContainsString('msgid "double \\"quoted\\""', $result, 'Strings with quotes not handled correctly');
+        $this->assertStringContainsString("msgid \"single 'quoted'\"", $result, 'Strings with quotes not handled correctly');
+    }
+
+    /**
+     * testExecute with no paths
+     */
+    public function testExecuteNoOutputOption(): void
+    {
+        $this->exec(
+            'i18n extract ' .
+                '--merge=no ' .
+                '--extract-core=no ' .
+                '--paths=' . TEST_APP . 'templates' . DS . 'i18n ' ,
+            [
+                $this->path,
+                TEST_APP . 'templates' . DS . 'i18n' . DS,
+                'D',
+            ],
+        );
+        $this->assertExitSuccess();
+        $this->assertFileExists($this->path . DS . 'default.pot');
+    }
+
+    /**
+     * testExecute with merging on method
+     */
+    public function testExecuteMerge(): void
+    {
+        $this->exec(
+            'i18n extract ' .
+                '--merge=yes ' .
+                '--extract-core=no ' .
+                '--paths=' . TEST_APP . 'templates' . DS . 'i18n ' .
+                '--output=' . $this->path . DS,
+            [
+                $this->path,
+            ]
+        );
+        $this->assertExitSuccess();
+        $this->assertFileExists($this->path . DS . 'default.pot');
+        $this->assertFileDoesNotExist($this->path . DS . 'cake.pot');
+        $this->assertFileDoesNotExist($this->path . DS . 'domain.pot');
+    }
+
+    /**
+     * test exclusions
+     */
+    public function testExtractWithExclude(): void
+    {
+        $this->exec(
+            'i18n extract ' .
+                '--extract-core=no ' .
+                '--exclude=Pages,Layout ' .
+                '--paths=' . TEST_APP . 'templates' . DS . ' ' .
+                '--output=' . $this->path . DS,
+        );
+        $this->assertExitSuccess();
+        $this->assertFileExists($this->path . DS . 'default.pot');
+        $result = file_get_contents($this->path . DS . 'default.pot');
+
+        $pattern = '/\#: .*extract\.php:\d+\n/';
+        $this->assertDoesNotMatchRegularExpression($pattern, $result);
+
+        $pattern = '/\#: .*default\.php:\d+\n/';
+        $this->assertDoesNotMatchRegularExpression($pattern, $result);
+    }
+}
