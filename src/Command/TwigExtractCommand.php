389efb7f08d461fb7b7994c6e03850b28607402d
Péter Nagy <peter@antronin.consulting>
2026-08-19 12:07:23 +0200

    Use FQCN For ProgressHelper as it has an alias

--- src/Command/TwigExtractCommand.php
+++ src/Command/TwigExtractCommand.php
@@ -61,7 +61,7 @@ class TwigExtractCommand extends I18nExtractCommand
     protected function _extractTokens(Arguments $args, ConsoleIo $io): void
     {
         $progress = $io->helper('Progress');
-        assert($progress instanceof ProgressHelper);
+        assert($progress instanceof \Cake\Console\Helper\ProgressHelper);
         $progress->init(['total' => count($this->_files)]);
         $isVerbose = $args->getOption('verbose');
 
